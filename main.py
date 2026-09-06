9207be1afe31be958ac6dbfb4d9a1b312bc0144b
Darkguitos <gabrielpaulino1011@gmail.com>
2026-09-06 17:44:34 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class listaduplamenteligada:
 
         atual = self.inicio
         valores = []
-      
+        
         while atual is not None:
             valores.append(str(atual.valor))
             atual = atual.proximo
@@ -26,7 +26,7 @@ class listaduplamenteligada:
 
     def inserir_final(self, valor):
         novo = no(valor)
-
+        
         if self.inicio is None:
             self.inicio = novo
             self.fim = novo
@@ -47,7 +47,7 @@ class listaduplamenteligada:
         novo.proximo = self.inicio
         self.inicio.anterior = novo
         self.inicio = novo
-
+    
     def inserir_meio(self, valor, posicao):
         if posicao <= 0 or self.inicio is None:
             self.inserir_inicio(valor)
@@ -55,7 +55,7 @@ class listaduplamenteligada:
 
         atual = self.inicio
         indice = 0
-        
+        #quer dizer que enquanto o atual nao for nulo e o indice for menor que a posicao, ele vai percorrer a lista ate chegar na posicao que ele quer
         while atual is not None and indice < posicao:
             atual = atual.proximo
             indice += 1 
@@ -63,7 +63,7 @@ class listaduplamenteligada:
         if atual is None:
             self.inserir_final(valor)
             return
-        
+        #essa parte abaixo faz o nos se ligarem, o proximo do anterior vai ser o novo e o proximo do novo vai ser o atual(nao pensa muito nisso, vai enquentar cabeça atoa)
         novo = no(valor)
         anterior = atual.anterior
         novo.anterior = anterior
@@ -83,12 +83,12 @@ class listaduplamenteligada:
 
         self.inicio = self.inicio.proximo
         self.inicio.anterior = None
-
+    #aqui é a parte que remove o ultimo elemento da lista, o fim da lista
     def remover_final(self):
         if self.fim is None:
             print("nao da pra remover ta vazio.")
             return
-
+        
         if self.inicio == self.fim:
             self.inicio = None
             self.fim = None
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     lista.inserir_final(10)
     lista.inserir_final(20)
     lista.inserir_final(30)
-    lista.imprimir() 
+    lista.imprimir() #essa parte tem que ficar vazia pois vai ser inserida nela os valores 10, 20 e 30
 
     lista.inserir_inicio(5)
     lista.imprimir()  
